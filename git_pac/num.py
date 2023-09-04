ad5d8cc7261c80747fd0a6846c7de31332cd48d9
Admin <nurbol1314@gmail.com>
2023-09-04 15:23:40 +0600

    init

--- git_pac/num.py
+++ git_pac/num.py
@@ -3,10 +3,9 @@ def plus(a, b):
 def min(a, b):
     return a - b
 
-def max(a, b):
-    return a * b
-
+def jj(a, b):
+    return a * b + 20
 
 print(plus(5, 2))
 print(min(10, 6))
-print(max(10, 6))
+print(jj(10, 6))
